8d1e63d8c4f6658ad0b81a50adcb36a7f6301116
Your Lord and Master <kuriosly@gmail.com>
2021-03-25 18:14:52 -0700

    Division Commands Added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { DivisionCommandHelper } from "./commands/division";
 import { CommonUtil } from "./general/common";
 import { DiscordBot } from "./general/discordBot";
 
 CommonUtil.init();
 var bot = new DiscordBot();
+DivisionCommandHelper.addCommands(bot);
 bot.login();
