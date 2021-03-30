9b7062d4119436695cddf0a8803d9954fe1bf3cd
Your Lord and Master <kuriosly@gmail.com>
2021-03-29 17:45:58 -0700

    logging

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,22 @@ import { DiscordBot } from "./general/discordBot";
 
 import * as smp from 'source-map-support';
 import * as http from 'http';
+import { Logs } from "./general/logs";
 
 
 smp.install();
 
 CommonUtil.init();
+Logs.log("Starting Bot");
 const bot = new DiscordBot();
 DivisionCommandHelper.addCommands(bot);
 MiscCommandHelper.addCommands(bot);
 MapCommandHelper.addCommands(bot);
 bot.login();
 const healthcheck = http.createServer(function (req,res){
+    Logs.log(req);
     res.write("pong");
     res.end();
 });
+Logs.log("Starting healthcheck server on 8080")
 healthcheck.listen(8080);
