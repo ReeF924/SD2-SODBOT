90c4baed4d843b07161d02e533dbc6e89fefd86c
Your Lord and Master <kuriosly@gmail.com>
2021-03-30 18:01:46 -0700

    purge

--- src/results/fetchPlayerData.js
+++
@@ -1,9 +0,0 @@
-const { sql } = require("../general/common");
-
-module.exports.fetchPlayerData = async playerEugenUID => {
-  return await sql(
-    `SELECT * FROM players WHERE "${playerEugenUID}" IN(EugenUID, DiscordUID)`
-  ).catch(() => {
-    console.log("Caught promise error");
-  });
-};
